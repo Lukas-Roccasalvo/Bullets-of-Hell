e88eb5ffabd7819ca5ebde561e5c8c3d6b212500
poly-arch <66742989+poly-arch@users.noreply.github.com>
2022-06-30 17:32:05 +0200

    Buggy spawner variants

--- Bullets-of-Hell/Assets/Bullet_movement.cs
+++ Bullets-of-Hell/Assets/Bullet_movement.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Random_linear_bullet_movement : MonoBehaviour
+public class Bullet_movement : MonoBehaviour
 {
-    private Vector2 direction;
+    public Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
-        direction = new Vector2(Random.value * 10 - 5, Random.value * 10 - 5);
 
 
     }
--- Bullets-of-Hell/Assets/SpawnerScript.cs
+++ Bullets-of-Hell/Assets/SpawnerScript.cs
@@ -6,34 +6,83 @@ public class SpawnerScript : MonoBehaviour
 {
     public GameObject bullet;
     public SpawnerScript.Type type;
-    private float spawnIntervall = 0f;
+    public bool randomPosition;
+
+    private float spawnTimeLeft = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector2(Random.Range(-50f, 50), Random.Range(-28f, 28));
-
+        if (randomPosition)
+        {
+            transform.position = new Vector2(Random.Range(-50f, 50), Random.Range(-28f, 28));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawnIntervall -= Time.deltaTime;
-        if(spawnIntervall < 0)
+        spawnTimeLeft -= Time.deltaTime;
+        if(spawnTimeLeft < 0)
         {
-            spawnIntervall += 0.5f;
             spwawnBullet(type);
         }
     }
 
     void spwawnBullet(SpawnerScript.Type type)
     {
-        Instantiate(bullet, this.transform);
+        GameObject newBullet = Instantiate(bullet, this.transform);
+        Vector2 direction = new Vector2();
+        switch (type)
+        {
+            case Type.random_radial:
+                direction = new Vector2(Random.value * 10 - 5, Random.value * 10 - 5);
+                spawnTimeLeft += 0.5f;
+                break;
+            case Type.random_left:
+                direction = new Vector2(Random.Range(-10f, -5f), 0f);
+                newBullet.GetComponent<SpriteRenderer>().color = new Color(0f, 200f, 0f);
+                newBullet.GetComponent<TrailRenderer>().startColor = new Color(0f, 200f, 0f, 0.6f);
+                newBullet.GetComponent<TrailRenderer>().endColor = new Color(0f, 200f, 0f, 0f);
+                spawnTimeLeft += 2f;
+                break;
+            case Type.random_right:
+                direction = new Vector2(Random.Range(10f, 5f), 0f);
+                newBullet.GetComponent<SpriteRenderer>().color = new Color(0f, 200f, 0f);
+                newBullet.GetComponent<TrailRenderer>().startColor = new Color(0f, 200f, 0f, 0.6f);
+                newBullet.GetComponent<TrailRenderer>().endColor = new Color(0f, 200f, 0f, 0f);
+                spawnTimeLeft += 2f;
+                break;
+            case Type.random_up:
+                direction = new Vector2(0f, Random.Range(10f, 5f));
+                newBullet.GetComponent<SpriteRenderer>().color = new Color(0f, 200f, 0f);
+                newBullet.GetComponent<TrailRenderer>().startColor = new Color(0f, 200f, 0f, 0.6f);
+                newBullet.GetComponent<TrailRenderer>().endColor = new Color(0f, 200f, 0f, 0f);
+                spawnTimeLeft += 2f;
+                break;
+            case Type.random_down:
+                direction = new Vector2(0f, Random.Range(-10f,-5f));
+                newBullet.GetComponent<SpriteRenderer>().color = new Color(0f, 200f, 0f);
+                newBullet.GetComponent<TrailRenderer>().startColor = new Color(0f, 200f, 0f, 0.6f);
+                newBullet.GetComponent<TrailRenderer>().endColor = new Color(0f, 200f, 0f, 0f);
+                spawnTimeLeft += 2f;
+                break;
+            default:
+                break;
+        }
+
+        newBullet.GetComponent<Bullet_movement>().direction = direction;
+
+
     }
 
     public enum Type
     {
         random_radial,
+        random_left,
+        random_right,
+        random_up,
+        random_down
         
     }
 }
