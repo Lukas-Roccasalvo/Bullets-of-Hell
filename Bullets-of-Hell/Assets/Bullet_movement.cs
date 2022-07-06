981f33a291eef7feaa6dd8a18070f90ed503e980
Lukas-Roccasalvo <roccasalvo.lukas@gmail.com>
2022-07-06 23:38:17 +0200

    collider vergrößert und bullets richtig gelöscht

--- Bullets-of-Hell/Assets/Bullet_movement.cs
+++ Bullets-of-Hell/Assets/Bullet_movement.cs
@@ -9,7 +9,6 @@ public class Bullet_movement : MonoBehaviour
     void Start()
     {
 
-
     }
 
     // Update is called once per frame
@@ -20,9 +19,10 @@ public class Bullet_movement : MonoBehaviour
         if(transform.position.x > 100f || transform.position.x < -100f ||
             transform.position.y > 50f || transform.position.y < -50f)
         {
-            Destroy(this);
+            Destroy(gameObject);
         }
+        
     }
 
-   
+
 }
