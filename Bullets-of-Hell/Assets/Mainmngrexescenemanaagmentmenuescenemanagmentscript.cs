aaf34dbbe5f4cadbb038f8bcb55ca862c4649429
poly-arch <66742989+poly-arch@users.noreply.github.com>
2022-07-08 01:45:19 +0200

    mitten drin aufghert

---
+++ Bullets-of-Hell/Assets/Mainmngrexescenemanaagmentmenuescenemanagmentscript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmngrexescenemanaagmentmenuescenemanagmentscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void lvl0()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void lvl1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void lvl2()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void lvl3()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void lvl1()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void lvl1()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void lvl1()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void lvl1()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
